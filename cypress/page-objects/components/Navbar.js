634d980f7a9f97f435e2e22d7641d29f8685f51b
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-12 12:43:39 -0700

    Add Navbar component

---
+++ cypress/page-objects/components/Navbar.js
@@ -0,0 +1,43 @@
+class Navbar {
+    static get logo() {
+        return cy.get(".brand-title");
+    }
+
+    static get searchBar() {
+        return cy.get(".mat-mdc-autocomplete-trigger");
+    }
+
+    static get cartButton() {
+        return cy.get(
+            '[ng-reflect-router-link="/shopping-cart"] > .mat-mdc-button-touch-target'
+        );
+    }
+
+    static get loginButton() {
+        return cy.get(".ng-star-inserted > .mat-mdc-button-touch-target");
+    }
+
+    static get wishListButton() {
+        return cy.get(
+            ".mdc-icon-button.ng-star-inserted > .mat-mdc-button-touch-target"
+        );
+    }
+
+    static get userMenu() {
+        return cy.get(".mat-mdc-menu-trigger > .mat-mdc-button-touch-target");
+    }
+
+    static get username() {
+        return cy.get(".mat-mdc-menu-trigger > .mdc-button__label > span");
+    }
+
+    static get myOrdersButton() {
+        return cy.get('[ng-reflect-router-link="/myorders"]');
+    }
+
+    static get logoutButton() {
+        return cy.get(".mat-mdc-menu-content > :nth-child(2)");
+    }
+}
+
+export default Navbar;
