5a15bacc814df2df201e9b6e3daaab8d46c86709
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-12 13:04:38 -0700

    Add PriceFilter component

---
+++ cypress/page-objects/components/PriceFilter.js
@@ -0,0 +1,15 @@
+class PriceFilter {
+    static get priceSlider() {
+        return cy.get(".mdc-slider__input");
+    }
+
+    static get minimumPrice() {
+        return cy.get(".d-flex > :nth-child(1) > strong").invoke("text");
+    }
+
+    static get maximumPrice() {
+        return cy.get(":nth-child(3) > strong").invoke("text");
+    }
+}
+
+export default PriceFilter;
