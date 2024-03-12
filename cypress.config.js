cfb08764dfcc1ff76acd769113cd5f57f614668a
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-12 11:01:01 -0700

    Install mochawesome reporter and add baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    reporter: "cypress-mochawesome-reporter",
+    e2e: {
+        setupNodeEvents(on, config) {
+            require("cypress-mochawesome-reporter/plugin")(on);
+        },
+        baseUrl: "https://bookcart.azurewebsites.net/",
     },
-  },
 });
