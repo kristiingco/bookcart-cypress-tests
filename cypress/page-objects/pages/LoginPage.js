b04ffa9e03e9c663ddd3caf771d68f036a870e63
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-12 18:09:03 -0700

    Add LoginPage

---
+++ cypress/page-objects/pages/LoginPage.js
@@ -0,0 +1,21 @@
+import BasePage from "./BasePage";
+
+class LoginPage extends BasePage {
+    static get usernameField() {
+        return cy.contains("Username");
+    }
+
+    static get passwordField() {
+        return cy.contains("Password");
+    }
+
+    static get registerButton() {
+        return cy.contains("Register");
+    }
+
+    static login(username, password) {
+        this.usernameField.type(username);
+        this.passwordField.type(password);
+        this.registerButton.click();
+    }
+}
