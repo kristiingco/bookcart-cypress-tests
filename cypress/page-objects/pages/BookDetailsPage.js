9809af30ed94d3f21b6216c9eff077980ef64feb
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-13 14:34:37 -0700

    Add BookDetailsPage

---
+++ cypress/page-objects/pages/BookDetailsPage.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress"/>
+
+import BasePage from "./BasePage";
+
+class BookDetailsPage extends BasePage {
+    static get bookTitle() {
+        return cy.get("tbody > :nth-child(1) > :nth-child(2)").invoke("text");
+    }
+
+    static get bookAuthor() {
+        return cy.get("tbody > :nth-child(2) > :nth-child(2)").invoke("text");
+    }
+
+    static get bookCategory() {
+        return cy.get("tbody > :nth-child(3) > :nth-child(2)").invoke("text");
+    }
+
+    static get price() {
+        return cy.get("tbody > :nth-child(4) > :nth-child(2)").invoke("text");
+    }
+
+    static get addToCartButton() {
+        return cy.get("Add to Cart");
+    }
+
+    static get addTioWishlistButton() {
+        return cy.get("Add to Wishlist");
+    }
+}
+
+export default BookDetailsPage;
